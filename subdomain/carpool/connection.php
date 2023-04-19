ecea6235da55d72e755f0d7ba5b539aefb46c972
lanceivo <samsonlance1@gmail.com>
2023-04-19 20:18:12 +0800

    mail update

--- subdomain/carpool/connection.php
+++ subdomain/carpool/connection.php
@@ -1,11 +1,11 @@
 <?php
 
-        $server = "localhost";
+        //$server = "localhost";
         $username = "u235219407_lance";
         $password = "Lance@15";
         $database = "u235219407_carpooling";
     
-    $db_connection = mysqli_connect($server,$username, $password,$database);
+    $db_connection = mysqli_connect($username,$password,$database);
     if(mysqli_connect_errno()){
         echo "Failed to connect to MYSQL:" .mysqli_connect_error();
         exit();
