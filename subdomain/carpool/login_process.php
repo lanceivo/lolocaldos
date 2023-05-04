aa4d794d1ceb6e3d5f8ecf1157c3f22bc01a98a7
lanceivo <samsonlance1@gmail.com>
2023-05-04 21:59:38 +0800

    updated login process

--- subdomain/carpool/login_process.php
+++ subdomain/carpool/login_process.php
@@ -5,44 +5,33 @@ require 'connection.php';
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$_SESSION['email'] = $email;
-$_SESSION['password'] = $password;
-
-$query = "SELECT * FROM tbuser WHERE Email = '$email' AND Password = '$password' AND approved = 'Registered'";
+$query = "SELECT * FROM tbuser WHERE Email = '$email' AND Password = '$password'";
 $result = mysqli_query($db_connection, $query);
 
 if (mysqli_num_rows($result) > 0) {
     $row = $result->fetch_assoc();
-    $_SESSION['email'] = $email;
-    $_SESSION['password'] = $password;
-    
-    $ids = $row['User_ID'];
-    $_SESSION['uid'] = $ids;
-    header("Location:Homepage.php");
-    exit();
-} else{
-    $_SESSION['login_alert1'] = '2';
-    header("Location:Login.php"); 
-    
-}
-$email2 = 'admin@gmail.com';
-$password2 = 'admin';
-$query2 = "SELECT * FROM tbuser WHERE Email = '$email2' AND Password = '$password2' AND approved = 'Admin'";
-$result2 = mysqli_query($db_connection, $query2);
-
-if (mysqli_num_rows($result2) > 0) {
-    $row = $result2->fetch_assoc();
-    $_SESSION['email'] = $email;
-    $_SESSION['password'] = $password;
-
-    $ids = $row['User_ID'];
-    $_SESSION['uid'] = $ids;
-    header("Location:Homepage_Admin.php");
-    exit();
+    if ($row['approved'] == 'Registered') {
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+        $_SESSION['uid'] = $row['User_ID'];
+        header("Location:Homepage.php");
+        exit();
+    } else if ($row['approved'] == 'Admin' && $email == 'admin@gmail.com' && $password == 'admin') {
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+        $_SESSION['uid'] = $row['User_ID'];
+        header("Location:Homepage_Admin.php");
+        exit();
+    } else {
+        $_SESSION['login_alert1'] = '2';
+        header("Location:Login.php");
+        exit();
+    }
 } else {
     $_SESSION['login_alert1'] = '2';
-    header("Location:Login.php"); 
+    header("Location:Login.php");
     exit();
 }
+
 mysqli_close($db_connection);
 ?>
