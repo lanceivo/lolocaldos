b4cf29e47c45e0590b712a73cc10ba1a8ca8f95c
lanceivo <samsonlance1@gmail.com>
2023-05-04 12:01:00 +0800

    css

--- subdomain/carpool/index.php
+++ subdomain/carpool/index.php
@@ -7,7 +7,7 @@
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  <link href="css/style.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
 </head>
 <?php
     require 'Alert.php';
