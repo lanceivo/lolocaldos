0094ef052dd67b48d5a1b212df589509e496598e
lanceivo <samsonlance1@gmail.com>
2023-05-08 14:47:54 +0800

    updated

--- subdomain/carpool/Alert.php
+++ subdomain/carpool/Alert.php
@@ -4,9 +4,6 @@
     if (isset($_SESSION['Email_exist']) && $_SESSION['Email_exist'] == 1) {
       $message = '<div id="incorrectMessage">Email is Already Exist</div><script>setTimeout(function(){ document.getElementById("incorrectMessage").style.display = "none"; }, 8000);</script>';
       unset($_SESSION['Email_exist']);
-    }else if(isset($_SESSION['login_alert1']) && $_SESSION['login_alert1'] == 2){
-        $message = '<div id="incorrectMessage">Email and Password is Incorrect</div><script>setTimeout(function(){ document.getElementById("incorrectMessage").style.display = "none"; }, 8000);</script>';
-        unset($_SESSION['login_alert1']);
     }else if(isset($_SESSION['car_alert']) && $_SESSION['car_alert'] == 3){
         $message = '<div id="incorrectMessage">Wait for your Car Approval</div><script>setTimeout(function(){ document.getElementById("incorrectMessage").style.display = "none"; }, 8000);</script>';
       unset($_SESSION['car_alert']);
