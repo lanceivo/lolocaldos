8575e42c0f08a6bb85b1741360bf50194520b035
lanceivo <samsonlance1@gmail.com>
2023-05-08 13:14:39 +0800

    updated logout

--- subdomain/carpool/Homepage.php
+++ subdomain/carpool/Homepage.php
@@ -33,6 +33,14 @@
     </div>
   </nav>
   <?php
+    session_start(); 
+    if(isset($_SESSION['email'])) {
+        $email = $_SESSION['email'];
+    }else{
+        header('Location: index.php');
+    }   
+?>
+  <?php
                     require 'connection.php';
                     $email = $_SESSION["email"];
                     
--- subdomain/carpool/Homepage_Admin.php
+++ subdomain/carpool/Homepage_Admin.php
@@ -55,6 +55,14 @@
     </div>
   </nav>
   <?php
+    session_start(); 
+    if(isset($_SESSION['email'])) {
+        $email = $_SESSION['email'];
+    }else{
+        header('Location: index.php');
+    }   
+?>
+  <?php
                    require 'connection.php';
                     $email = $_SESSION["email"];
                     $password = $_SESSION["password"];   
