5c853103337abbde0619dc3671a1a5e16be8cae4
lanceivo <samsonlance1@gmail.com>
2023-05-20 20:57:00 +0800

    cico container

--- subdomain/carpool/CICO.php
+++ subdomain/carpool/CICO.php
@@ -55,5 +55,77 @@
               <center> <h5 style="color: red; top:10px "><?php if (isset($message)) { echo $message; } ?></h5></center>
         </form>
     </div>
+    <script>
+   // Get the amount input field
+const amountInput = document.querySelector('input[name="amount"]');
+// Get the conversion fee input field
+const conversionFeeInput = document.querySelector('input[name="confee"]');
+
+// Listen for changes in the amount input field
+amountInput.addEventListener('input', function() {
+    // Get the amount value
+    const amount = parseInt(this.value);
+    
+    // Calculate the conversion fee based on the amount
+    let conversionFee = 0;
+    if (amount >= 50) {
+        if (amount >= 50 && amount <= 99) {
+            conversionFee = 40;
+        } else if (amount >= 100 && amount <= 249) {
+            conversionFee = 80;
+        } else if (amount >= 250 && amount <= 499) {
+            conversionFee = 200;
+        } else if (amount >= 500) {
+            conversionFee = 450;
+        }
+    }
+    // Set the conversion fee value in the input field
+    conversionFeeInput.value = conversionFee;
+});
+
+</script>
+<script>
+   // Get the amount input field
+const amtInput = document.querySelector('input[name="amount"]');
+// Get the conversion fee input field
+const processingFeeInput = document.querySelector('input[name="profee"]');
+
+// Listen for changes in the amount input field
+amtInput.addEventListener('input', function() {
+    // Get the amount value
+    const amount = parseInt(this.value);
+    
+    // Calculate the processing fee based on the amount
+    let processingFee = 0;
+    if (amount >= 50) {
+        const thousandBlocks = Math.floor(amount / 1000);
+        processingFee = thousandBlocks * 20;
+    }
+
+    // Set the processing fee value in the input field
+    processingFeeInput.value = processingFee;
+});
+</script>
+<script>
+  function toggleFields() {
+    var transactionType = document.getElementById("cico").value;
+    var amountField = document.getElementById("amountField");
+    var gcashRefField = document.getElementById("gcashRefField");
+    var processingFeeField = document.getElementById("processingFeeField");
+    var conversionFeeField = document.getElementById("conversionFeeField");
+
+    if (transactionType === "CashIn") {
+      amountField.style.display = "block";
+      gcashRefField.style.display = "block";
+      processingFeeField.style.display = "none";
+      conversionFeeField.style.display = "none";
+    } else if (transactionType === "CashOut") {
+      amountField.style.display = "block";
+      gcashRefField.style.display = "none";
+      processingFeeField.style.display = "block";
+      conversionFeeField.style.display = "block";
+    }
+  }
+</script>
 </body>
 </html>
