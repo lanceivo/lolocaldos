77bcc0cebb30508dd1a16f90131763221ad11e81
lanceivo <samsonlance1@gmail.com>
2023-05-20 20:13:54 +0800

    cashin div

--- subdomain/carpool/cashin_passenger.php
+++ subdomain/carpool/cashin_passenger.php
@@ -15,5 +15,44 @@
         <span style="color:#ffffff; font-size:26px; font-weight:bold; letter-spacing: 1px;">Carpool App</span>
       </a>
   </nav>
+  <div id="container" style="margin-top: 8%;">
+
+  <?php
+    session_start();
+    require 'connection.php';
+    $email = $_SESSION["email"];
+    $password = $_SESSION["password"];   
+    $sql = "SELECT * FROM tbuser WHERE Email= '$email' AND Password ='$password'";
+    $result = mysqli_query($db_connection, $sql);
+    $row = mysqli_fetch_assoc($result);
+  ?>
+  <h1 style="color: black;" id="welcome">Welcome <span style="color: black;"><?php echo $row['Firstname']." ".$row['Lastname']; ?></span></h1>
+  <h1 style="color: black;" id="welcome">Balance <span style="color: black;"><?php echo $row['uBalance'] ?></span></h1>
+
+  <h2 style="text-align: center;">MODE OF TRANSACTION</h2>
+
+  <form action="cashin_process.php" method="post">
+            <label for="cico">Transaction Type</label>
+            <select id="cico" name="cico">
+            <option value="CashIn">Cash In</option>
+            </select><br>
+            <div id="amountField">
+            <label for="amount">Amount</label><br>
+                <select name="amount" required>
+                    <option value="">Select an amount</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                    <option value="250">250</option>
+                    <option value="500">500</option>
+                </select><br>
+            </div>
+            <div id="gcashRefField">
+            <label for="gcashref">Gcash Reference</label><br>
+            <input type="number" name="gcashref" required><br>
+            </div>
+            <input type="submit" name="proceed" value="Proceed" id="proceed">
+            <center> <h6 style="color: red; top:10px "><?php if (isset($message)) { echo $message; } ?></h6></center>  
+  </form>  
+</div>
 </body>
 </html>
