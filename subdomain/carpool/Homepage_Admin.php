6c6c64299ad809ea977105d148504d5d66786125
lanceivo <samsonlance1@gmail.com>
2023-05-20 17:05:20 +0800

    css update

--- subdomain/carpool/Homepage_Admin.php
+++ subdomain/carpool/Homepage_Admin.php
@@ -7,7 +7,7 @@
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link href="css/car_registration.css" rel="stylesheet">
+    <link href="css/car.registration.css" rel="stylesheet">
 </head>
 <style>
               table {
