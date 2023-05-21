120d0a8a0d102c5741ec82f9d634705b699f7a34
lanceivo <samsonlance1@gmail.com>
2023-05-21 19:05:25 +0800

    css admin

--- subdomain/carpool/Homepage_Admin.php
+++ subdomain/carpool/Homepage_Admin.php
@@ -8,7 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Signika+Negative&display=swap" rel="stylesheet">
-    <link href="css/car_registration.css" rel="stylesheet">
+    <link href="css/car.registration.css" rel="stylesheet">
     <link rel="icon" type="png" href="img/s1.png">
 
 </head>
