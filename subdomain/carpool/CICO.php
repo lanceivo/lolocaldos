28cb0d82afb94e77f0945cc219da743ba4ae4564
lanceivo <samsonlance1@gmail.com>
2023-05-21 12:58:20 +0800

    logo

--- subdomain/carpool/CICO.php
+++ subdomain/carpool/CICO.php
@@ -7,6 +7,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="css/cico.css">
+    <link rel="icon" type="png" href="img/s1.png">
+
   <title>CASH IN / CASH OUT</title>
 </head>
 <body>
