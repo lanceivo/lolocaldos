c3e01fec1abfee2585f26050a48b571b58005c17
lanceivo <samsonlance1@gmail.com>
2023-06-09 00:14:19 +0800

    forgot password

---
+++ forgotpassword.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Forgot-Password-LoloCaldos</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link rel="icon" type="png" href="assets/img/logo.png">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/login_register.css">
+</head>
+
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="index.html">LOLO CALDOS</a></h1>
+
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a  href="index.html">Home</a></li>
+          <li><a href="service.php">Services</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="gallery.html">Gallery</a></li>
+          <li class="dropdown"><a href="#" class="active"><span>Admin</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="login_register.html">Login</a></li>       
+            </ul>
+          </li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- LOGIN | REGISTER  -->
+
+  <div class="section">
+    <div class="container" style="margin-top: 5%;">
+        <div class="row full-height justify-content-center">
+            <div class="col-12 text-center align-self-center py-5">
+                <div class="section pb-5 pt-5 pt-sm-2 text-center">
+                    <form action="forgotpassword_process.php" method="post">
+                        <h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+                        <input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+                        <label for="reg-log"></label>
+                      <div class="card-3d-wrap mx-auto">
+                          <div class="card-3d-wrapper">
+                              <div class="card-front">
+                                  <div class="center-wrap">
+                                      <div class="section text-center">
+                                          <h4 class="mb-4 pb-3">Forgot Password</h4>
+                                          <div class="form-group">
+                                              <input type="email" name="email" class="form-style" placeholder="Enter your email address">
+                                              <i class="input-icon uil uil-at"></i>
+                                          </div>	
+                                          <button type="submit"class="btn mt-4" name="forgotpassword" >Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                              <div class="card-back">
+                                  <div class="center-wrap">
+                                      <div class="section text-center">
+                                          <h4 class="mb-3 pb-3">Sign Up</h4>
+                                          <div class="form-group">
+                                              <input type="text" class="form-style" name="" placeholder="Full Name">
+                                              <i class="input-icon uil uil-user"></i>
+                                          </div>	
+                                          <div class="form-group mt-2">
+                                              <input type="tel" class="form-style" name="" placeholder="Phone Number">
+                                              <i class="input-icon uil uil-phone"></i>
+                                          </div>	
+                    <div class="form-group mt-2">
+                                              <input type="email" class="form-style" name="" placeholder="Email">
+                                              <i class="input-icon uil uil-at"></i>
+                                          </div>
+                                          <div class="form-group mt-2">
+                                              <input type="password" class="form-style" name="" placeholder="Password">
+                                              <i class="input-icon uil uil-lock-alt"></i>
+                                          </div>
+                                          <button class="btn mt-4">Register</button>
+                                        </div>
+                    </form>
+
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+    </div>
+</div>
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>LOLO CALDOS</h3>
+      <p>Experience the warm hospitality and welcoming atmosphere that defines Lolo Caldos Resort. Allow us to create cherished memories and make your stay an unforgettable one. Welcome to a world of luxury, serenity, and unparalleled beauty. Welcome to Lolo Caldos Resort, your home away from home.</p>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="https://www.facebook.com/LOLOCALDOSFARMVILLERESORT" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+      <div class="copyright">
+        &copy; Copyright <strong><span>Lolo Caldos Farmville Resort</span></strong>.
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ forgotpassword_process.php
@@ -0,0 +1,160 @@
+<?php
+    require 'connection.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+    require 'phpmailer/src/SMTP.php';
+function send_password_reset($get_email,$token){
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'contactlolocaldos@gmail.com';
+    $mail->Password = 'gnlinwxqbjmdkmwa';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+    $mail->setFrom('contactlolocaldos@gmail.com', 'Lolo Caldos Farmville Resort');
+    $mail->addAddress($get_email,$token);
+
+    $mail->isHTML(true);
+    $mail->Subject = "Reset Password Notification";
+    $msg = "
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head>
+            <meta charset='UTF-8'>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    line-height: 1.5;
+                    color: #333;
+                    margin: 0;
+                    padding: 20px;
+                }
+                
+                p {
+                    margin-bottom: 10px;
+                }
+                
+                strong {
+                    font-weight: bold;
+                }
+                
+                .signature {
+                    margin-top: 30px;
+                }
+                
+                .signature strong {
+                    font-size: 18px;
+                }
+            </style>
+        </head>
+        <body>
+            <p>Hello <strong>$get_email!</strong></p>
+            <p>Enter this code:  <strong>$token</strong></p>
+            <p>Code to Reset your password.</p>
+            <br><br>
+                <a href='http://localhost/lolocaldos/createpassword.php?token=$token&email=$get_email'>Verify Here</a>
+            </body>
+        </html>
+    ";
+    $mail->Body = $msg;
+    
+    // Call the send() function to send the email
+    $mail->send();
+}
+    if(isset($_POST['forgotpassword'])){
+        $email = mysqli_real_escape_string($db_connection,$_POST['email']);
+        $token = md5(rand());
+
+        $check_email = "SELECT email FROM tbuser WHERE email = '$email'";
+        $check_email_run = mysqli_query($db_connection,$check_email);
+
+        if(mysqli_num_rows($check_email_run)>0){
+            $row = mysqli_fetch_array($check_email_run);
+            $get_email= $row['email'];
+            $update_code = "UPDATE tbuser SET code='$token' WHERE email='$get_email' LIMIT 1";
+            $update_code_run = mysqli_query($db_connection,$update_code);
+
+            if($update_code_run){
+                    send_password_reset($get_email,$token);
+                    echo '<script>
+            window.alert("Password Emailed to you. ");
+            window.location.href ="forgotpassword.php";
+            </script>';
+            
+            exit();
+            }else{
+                echo '<script>
+            window.alert("Something went wrong");
+            window.location.href ="forgotpassword.php";
+            </script>';
+            exit();
+            }
+
+        }else{
+            echo '<script>
+            window.alert("No Email Found");
+            window.location.href ="forgotpassword.php";
+            </script>';
+            exit();
+        }
+    }
+
+    if(isset($_POST['password_update'])){
+        $email = mysqli_real_escape_string($db_connection,$_POST['email']);
+        $new = mysqli_real_escape_string($db_connection,$_POST['new_password']);
+        $confirm = mysqli_real_escape_string($db_connection,$_POST['confirm_password']);
+        $token = mysqli_real_escape_string($db_connection,$_POST['token']);
+
+        if(!empty($token)){
+            if(!empty($email) && !empty($new) && !empty($confirm)){
+                $check_token = "SELECT code FROM tbuser WHERE code='$token' LIMIT 1";
+                $check_token_run = mysqli_query($db_connection,$check_token);
+                if(mysqli_num_rows($check_token_run)){
+                          if($new == $confirm){
+                                $update_password = "UPDATE tbuser SET password ='$new' WHERE code='$token' LIMIT 1";
+                                $update_password_run = mysqli_query($db_connection,$update_password);
+                                if($update_password_run){
+                                    echo "<script>
+                                    window.alert('New Password Updated');
+                                    window.location.href ='login_register.html';
+                                    </script>";
+                                   
+                                }else{
+                                    echo "<script>
+                                    window.alert('Did not update password. Something went wrong.);
+                                    window.location.href ='createpassword.php?token=$token&email=$email';
+                                    </script>";
+                                }
+                          } else{
+                            echo "<script>
+                            window.alert('Password and Confirm Password not Match');
+                            window.location.href ='createpassword.php?token=$token&email=$email';
+                            </script>"; 
+                          } 
+                }else{
+                    echo "<script>
+                            window.alert('Invalid Token');
+                            window.location.href ='createpassword.php?token=$token&email=$email';
+                            </script>";   
+                }
+            }else{
+                echo "<script>
+                            window.alert('All Field is Required');
+                            window.location.href ='createpassword.php?token=$token&email=$email';
+                            </script>"; 
+            }
+        }else{
+            echo "<script>
+            window.alert('No Token Available');
+            window.location.href ='createpassword.php?token=$token&email=$email';
+            </script>"; 
+        }
+
+    }
+
+?>
